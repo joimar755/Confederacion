527f18cfe5a830b45f5d6d28f9593f5926c528dc
Joimar <52583649+joimar755@users.noreply.github.com>
2024-11-14 20:58:16 +0000

    plantas

--- plantas.py
+++ plantas.py
@@ -3,23 +3,23 @@ import pulp
 
 
 Q = pulp.LpProblem(
-    "SERVIDORES", pulp.LpMinimize
+    "Plantas", pulp.LpMinimize
 )  # si el problema es de Minimización se debe usar pulp.LpMinimize
 
 
 
-x1 = pulp.LpVariable("x1", cat="Binary")
-x2 = pulp.LpVariable("x2", cat="Binary")
-x3 = pulp.LpVariable("x3", cat="Binary")
-x4 = pulp.LpVariable("x4", cat="Binary")
-x5 = pulp.LpVariable("x5", cat="Binary")
-x6 = pulp.LpVariable("x6", cat="Binary")
-x7 = pulp.LpVariable("x7", cat="Binary")
-x8 = pulp.LpVariable("x8", cat="Binary")
-x9 = pulp.LpVariable("x9", cat="Binary")
-x10 = pulp.LpVariable("x10", cat="Binary")
-x11 = pulp.LpVariable("x11", cat="Binary")
-x12 = pulp.LpVariable("x12", cat="Binary")
+x1 = pulp.LpVariable("x1-1", cat="Binary")
+x2 = pulp.LpVariable("x1-2", cat="Binary")
+x3 = pulp.LpVariable("x1-3", cat="Binary")
+x4 = pulp.LpVariable("x1-4", cat="Binary")
+x5 = pulp.LpVariable("x2-1", cat="Binary")
+x6 = pulp.LpVariable("x2-2", cat="Binary")
+x7 = pulp.LpVariable("x2-3", cat="Binary")
+x8 = pulp.LpVariable("x2-4", cat="Binary")
+x9 = pulp.LpVariable("x3-1", cat="Binary")
+x10 = pulp.LpVariable("x3-2", cat="Binary")
+x11 = pulp.LpVariable("x3-3", cat="Binary")
+x12 = pulp.LpVariable("x3-4", cat="Binary")
 
 
 
@@ -32,59 +32,39 @@ Q += (
     + 960 * x4
     + 800 * x5
     + 870 * x6
-    + 1000000 * x7
     + 920 * x8
     + 740 * x9
     + 900 * x10
     + 810 * x11
     + 840 * x12
-   
 )
 
-nombres_servidores = [ "PC IPE", "PC IPM", "ET SGI", "ET SUN"]
+
 
 # Restricciones de la producción
-Q += 41 * x1 + 27 * x2 + 28 * x3 + 24 * x4 = 75
-Q += 30 * x5 + 80 * x6 + 200 * x7 + 2000 * x8 >= 0
-Q += 30 * x9 + 80 * x10 + 200 * x11 + 2000 * x12 >= 0
+# Restricciones de capacidad para cada planta
+Q += 41 * x1 + 27 * x2 + 28 * x3 + 24 * x4 <= 75    # Planta 1
+Q += 40 * x5 + 29 * x6 + 23 * x8 <= 75              # Planta 2
+Q += 37 * x9 + 30 * x10 + 27 * x11 + 21 * x12 <= 45 # Planta 3
 
-#Q += 30 * x1 + 80 * x2 + 200 * x3 + 2000 * x4 >= 0
-#Q += 30 * x1 + 80 * x2 + 200 * x3 + 2000 * x4 >= 0
-#Q += 30 * x1 + 80 * x2 + 200 * x3 + 2000 * x4 >= 0
-
-
-# Q += 3 * x1 + 2 * x2 + x3 <= 600, "Restricción de agua Kibbutz 1"
-# Q += 3 * x4 + 2 * x5 + x6 <= 800, "Restricción de agua Kibbutz 2"
-# Q += 3 * x7 + 2 * x8 + x9 <= 375, "Restricción de agua Kibbutz 3"
-#
-## Restricción total de acres para cada cultivo
-# Q += x1 + x4 + x7 <= 600, "Restricción total de acres para cultivo 1"
-# Q += x2 + x5 + x8 <= 500, "Restricción total de acres para cultivo 2"
-# Q += x3 + x6 + x9 <= 325, "Restricción total de acres para cultivo 3"
+Q += x1 + x5 + x9 == 1   #Producto 1 debe asignarse a alguna planta
+Q += x2 + x6 + x10 == 1  #Producto 2 debe asignarse a alguna planta
+Q += x3 + x11 == 1       #Producto 3 debe asignarse a alguna planta
+Q += x4 + x8 + x12 == 1  #Producto 4 debe asignarse a alguna planta
 
 
-# Valores para x1
+
 
 
 # Solucionar el problema
 Q.solve()
+for variable in Q.variables():
+    print(f"{variable.name} = {variable.varValue}")
+
+# Iterar sobre cada variable y verificar si su valor es 1
 
 
 print(f"Estado: {pulp.LpStatus[Q.status]}")
-# Imprimir los valores de las variables para cada servidor en cada mes
-# Imprimir los valores de las variables para cada servidor en cada mes
-for i in range(1, 21):
-    if i <= 4:
-        print(f"{nombres_servidores[i-1]} (Servidor Mes 1 - x{i}): {pulp.value(locals()[f'x{i}'])}")
-    elif 5 <= i <= 8:
-        print(f"{nombres_servidores[i-5]} (Servidor Mes 2): {pulp.value(locals()[f'x{i}'])}")
-        
-    elif 9 <= i <= 12:
-        print(f"{nombres_servidores[i-9]} (Servidor Mes 3): {pulp.value(locals()[f'x{i}'])}")
-    elif 13 <= i <= 16:
-        print(f"{nombres_servidores[i-13]} (Servidor Mes 4): {pulp.value(locals()[f'x{i}'])}")
-    else:
-        print(f"{nombres_servidores[i-17]} (Servidor Mes 5): {pulp.value(locals()[f'x{i}'])}")
 
 
 # Si tienes más meses, simplemente agregas las impresiones correspondientes
